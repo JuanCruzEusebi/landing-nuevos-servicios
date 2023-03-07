1f131abdf70208b6d65865c23c39710a6c2b7eff
JuanCruzEusebi <juancruzeusebi@gmail.com>
2023-03-07 12:24:06 -0300

    working on services styling

--- app/services/ServiceCard.jsx
+++ app/services/ServiceCard.jsx
@@ -5,9 +5,14 @@ export default function ServiceCard({ data }) {
     <div className={style.mainContainer}>
       {data.map((item, index) => {
         return (
-          <div key={index} className={style.card}>
-            <img src={item.image} alt="img" as={index} className={style.img} />
+          <div
+            key={index}
+            className={style.card}
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            {/* <img src={item.image} alt="img" as={index} className={style.img} /> */}
             <h1 className={style.heading}> {item.title}</h1>
+            <p className={style.para}>{item.title}</p>
           </div>
         );
       })}
